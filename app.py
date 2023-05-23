8dfc335bcd8939d8b08c11a59f59cf46f16675ed
star-bits <star-bits@outlook.com>
2023-05-23 19:42:04 +0900

    first chat test

--- app.py
+++ app.py
@@ -1,24 +1,17 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO, send
-import random
-import string
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 @app.route('/')
-def index():
+def chat():
     return render_template('index.html')
 
 @socketio.on('message')
 def handleMessage(msg):
     print('Message: ' + msg)
-    send(msg, broadcast=True)
-
-@socketio.on('connect')
-def handle_connect():
-    username = ''.join(random.choices(string.ascii_lowercase + string.digits, k=8))
-    send(username)
+    emit('message', msg, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app)
